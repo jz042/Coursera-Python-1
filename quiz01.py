416cfbfdc8bce1dca8c11f0a3021fb3f4fff223f
jz042 <juliezhou@Julies-MacBook-Air.local>
2015-02-27 07:23:36 -0800

    Week 1 files

---
+++ quiz01.py
@@ -0,0 +1,50 @@
+# Interactive Python Programming (Part 1)
+
+# Quiz 1
+
+# Q6
+
+def f(x):
+    y = -5*x^5 + 69*x^2 - 47
+    return y
+    
+print f(0)
+print f(1)
+print f(2)
+
+
+# Q7
+
+def future_value(present_value, annual_rate, periods_per_year, years):
+    rate_per_period = annual_rate / periods_per_year
+    periods = periods_per_year * years
+    pv = present_value
+    fv = pv
+    
+    for i in range(0,periods):
+        fv = pv * (1+rate_per_period)
+        pv = fv
+    return pv
+    
+print "$1000 at 2% compounded daily for 3 years yields $", future_value(1000, .02, 365, 3)
+    
+# Q8
+
+import math
+
+def polygon_area(n, s):
+	area = (.25 * n * s^2) / tan(pi/n)
+	return area
+	
+print polygon_area(7,5)
+
+# Q10
+
+import math
+
+def project_to_distance(point_x, point_y, distance):
+    dist_to_origin = math.sqrt(point_x ** 2 + point_y ** 2)    
+    scale = distance / dist_to_origin
+    print point_x * scale, point_y * scale
+    
+project_to_distance(2, 7, 4)
